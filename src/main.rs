9d70984ac1fb4e6e2072c2d7839b960850d12f1a
aadarsh214 <aadarshgupta7828@gmail.com>
2025-10-29 12:17:33 +0530

    Refactor input handling for improved readability in guessing game

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,27 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
-    println!(" input your guess:");
+        println!(" input your guess:");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
 
-    let guess: u32 = match guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-    
-        println!("You guessed: {guess}");
-    
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal =>{
-            println!("You win!");
-            break;
 
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
-        }
-}
+    }
 }
